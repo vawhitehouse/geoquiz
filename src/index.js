cf3891caa2d90dba15ce408584868b35a7e0d454
Valerie Whitehouse <vawhitehouse@yahoo.com>
2020-01-07 15:54:59 -0500

    works on europe map, general styling

--- src/index.js
+++ src/index.js
@@ -8,7 +8,22 @@ import Map from './scripts/map';
 window.addEventListener("DOMContentLoaded", () => {
   document
     .getElementById("europe")
-    .addEventListener("click", () => displayMap("continentsRussiaEuropeLow"));
+    .addEventListener("click", () => displayMap("europe"));
+  document
+    .getElementById("africa")
+    .addEventListener("click", () => displayMap("africa"));
+  document
+    .getElementById("asia")
+    .addEventListener("click", () => displayMap("asia"));
+  document
+    .getElementById("central-north-america")
+    .addEventListener("click", () => displayMap("centralNorthAmerica"));
+  document
+    .getElementById("south-america")
+    .addEventListener("click", () => displayMap("southAmerica"));
+  document
+    .getElementById("oceania")
+    .addEventListener("click", () => displayMap("oceania"));
 
   function displayMap(region) {
     document.getElementById("mapdiv")
--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,5 +1,3 @@
-
-
 export default class Map {
   constructor(region) {
     this.region = region;
@@ -15,12 +13,17 @@ export default class Map {
     map.projection = new am4maps.projections.NaturalEarth1();
     
     
-    
+
     // Create map polygon series
     let europeSeries = map.series.push(new am4maps.MapPolygonSeries());
     
     // Make map load polygon (like country names) data from GeoJSON
     europeSeries.useGeodata = true;
+
+    // ZOOM ON CLICK
+    // polygonSeries.mapPolygons.template.events.on("hit", function(ev) {
+    //   map.zoomToMapObject(ev.target);
+    // });
 
     // Configure series
     let europeTemplate = europeSeries.mapPolygons.template;
@@ -79,6 +82,54 @@ export default class Map {
       "UA",
       "VA",
       "XK"
+    ];
+
+    // europeTemplate.propertyFields.fill = "fill";
+
+    let imageSeries = map.series.push(new am4maps.MapImageSeries());
+
+    let imageSeriesTemplate = imageSeries.mapImages.template;
+    let circle = imageSeriesTemplate.createChild(am4core.Circle);
+    circle.radius = 4;
+    circle.fill = am4core.color("#e1e2e9");
+    circle.stroke = am4core.color("#FFFFFF");
+    circle.strokeWidth = 1;
+    circle.nonScaling = true;
+    circle.tooltipText = "{title}";
+
+    imageSeriesTemplate.propertyFields.latitude = "latitude";
+    imageSeriesTemplate.propertyFields.longitude = "longitude";
+    imageSeries.data = [
+      {
+        latitude: 43.9423599,
+        longitude: 12.457777,
+        title: "San Marino"
+      },
+      {
+        latitude: 41.902916,
+        longitude: 12.4533892,
+        title: "Vatican City State"
+      },
+      {
+        latitude: 42.5462456,
+        longitude: 1.601554,
+        title: "Andorra"
+      },
+      {
+        latitude: 47.1660004,
+        longitude: 9.5553732,
+        title: "Liechtenstein"
+      },
+      {
+        latitude: 43.7502975,
+        longitude: 7.4128408,
+        title: "Monaco"
+      },
+      {
+        latitude: 35.9374962,
+        longitude: 14.3754158,
+        title: "Malta"
+      }
     ];
 
   }
